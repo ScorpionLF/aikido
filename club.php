b321e104845503169574f05843cdd9c278f4660b
Dasha <ideliya3004@gmail.com>
2023-03-03 00:12:55 +0300

    Changed page 'About the club'

---
+++ club.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header_style.css">
+    <link rel="stylesheet" href="css/club.css">
+    <link rel="stylesheet" href="css/footer_style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@200&family=Inter&family=Roboto&display=swap"
+        rel="stylesheet">
+    <title>О клубе</title>
+</head>
+
+<body>
+    <?php include 'header.html'; ?>
+    <div class="about_the_club">
+        <div class="title-page">Ametsuchi Dojo</div>
+        <div class="yellow_line"></div>
+        <div class="info-page">
+            <p>Федерация Айкидо Ametsuchi Dojo входит во Всестилевую Федерацию Айкидо России. Поэтому все ученики
+                проходят
+                ежегодную аттестацию в Москве или Санкт-Петербурге. Также
+                Федерация Ametsuchi Dojo является организатором крупных международных фестивалей Айкидо в Казани, с
+                участием
+                ведущих сенсеев России и зарубежья. Каждый ученик проходит все мастер-классы данных фестивалей.
+            <p>Мы занимаемся обучением всех желающих уникальному искусству Айкидо. Уникальность заключается в том, что
+                вы
+                сможете побеждать не используя силу или агрессию. Именно поэтому Айкидо подходит для людей в любом
+                возрасте
+                и любой физической подготовке в том числе и для детей, ведь они смогут дать отпор противнику любого
+                возраста и комплекции,
+                старшекласснику и даже взрослому. На тренировках и соревнованиях не производится ударов по телу или по
+                голове, потому что Айкидо использует энергию противника против него самого.</p>
+            <p>Обучая вас самозащите, мы не воспитываем агрессию, не культивируем идею насилия в ответ насилию. Это
+                позволяет сохранять спокойствие и уверенность в любой ситуации, в духе наследия Великого Мастера Уэсиба
+                Морихей, который создал это единоборство после долгих лет совершенствования тела и духа.</p>
+            <p>Обучение проводят профессиональные инструкторы, которые входят в число мастеров Всестилевой федерации
+                Айкидо
+                России.</p>
+            <p>Открыты несколько секций, расположенных в г. Казани и г. Зеленодольске, в каждом из которых организованы
+                две группы:
+            <p>
+            <p>- детская (от 6 лет); </p>
+            <p>- взрослая (для всех возрастов от 14 лет); <br>
+                Занятия проводятся 3 раза в неделю.</p>
+        </div>
+        <img class="img_about_school" src="images/image.jpg" alt="about_school">
+    </div>
+    <?php include 'footer.html'; ?>
+</body>
+
+</html>
