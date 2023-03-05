58c975362e3cdf8516c69949e8b8d54f27494e90
Liniza Khatipova <fanilovna03@gmail.com>
2023-03-05 18:08:24 +0300

    ups

--- login_script.php
+++ login_script.php
@@ -1,37 +1,46 @@
 <?php session_start();?>
 <?php
-	extract($_POST);
-
-	if(isset($_POST['submit'])) {
-		$login = $_POST['login'];
-		$password = $_POST['password'];
-
-		if (empty($login) or empty($password)) {
-			echo "Введите логин и пароль.";
+	if (isset($_SESSION['login'])) {
+		$login = $_SESSION['login'];
+		if ($login == "admin") {
+			header('Location:admin_page.php');
 		} else {
-			// compare password with database
-			$file_arr = file("users/users.csv");
-    		$lines = count($file_arr);
+			header('Location:personal_page.php');
+		}
+	} else {
+		extract($_POST);
 
-			for ($i = 0; $i < $lines; $i++) {
-				echo $i;
-				$line = explode(';', $file_arr[$i]);
-				if (strval($line[0]) != strval($login)) {
-					if ($i + 1 == $lines) {
-						die("Неверный логин.");
-					}
-				} else {
-					if (strval($line[1]) != strval($password)) {
-						echo '_'.$line[1].'_'.$password.'_';
-						die("Неверный пароль.");
+		if(isset($_POST['submit'])) {
+			$login = $_POST['login'];
+			$password = $_POST['password'];
+
+			if (empty($login) or empty($password)) {
+				echo "Введите логин и пароль.";
+			} else {
+				// compare password with database
+				$file_arr = file("users/users.csv");
+				$lines = count($file_arr);
+
+				for ($i = 0; $i < $lines; $i++) {
+					// echo $i;
+					$line = explode(';', $file_arr[$i]);
+					if (strval($line[1]) != strval($login)) {
+						if ($i + 1 == $lines) {
+							die("Неверный логин.");
+						}
 					} else {
-						// setcookie("login", $login);
-	                    $_SESSION['login']=$login;
-
-						if ($login == "admin") {
-							header('Location:admin_page.php');
+						if (strval($line[2]) != strval($password)) {
+							// echo '_'.$line[2].'_'.$password.'_';
+							die("Неверный пароль.");
 						} else {
-							header('Location:personal_page.php');
+							// setcookie("login", $login);
+							$_SESSION['login']=$login;
+
+							if ($login == "admin") {
+								header('Location:admin_page.php');
+							} else {
+								header('Location:personal_page.php');
+							}
 						}
 					}
 				}
--- personal_page.php
+++ personal_page.php
@@ -23,31 +23,79 @@
 
 <body>
     <?php include 'header.html'; ?>
-    <div class="main">
+    <div class="personal_page">
         <div class="i_title_div">
             <div class="title_page_i">Личные данные</div>
-            <div class="line_line"></div>
+            <div class="yellow_line"></div>
         </div>
         <div class="block_personal_page">
-            <div class="foto_personal">
+            <div class="photo_personal">
                 <img class="img_person" src="images/image.jpg" alt="">
             </div>
             <div class="block_person">
                 <div class="fio">
                     <div class="request">ФИО:</div>
-                    <div class="info">Хатипова Линиза Фаниловна</div>
+                    <div class="info">
+                        <?php
+                        $login = $_SESSION['login'];
+                        $file_arr = file("users/data.csv");
+        				$lines = count($file_arr);
+                        for ($i = 0; $i < $lines; $i++) {
+                            $line = explode(';', $file_arr[$i]);
+        					if (strval($line[1]) == strval($login)) {
+                                echo $line[2].' '.$line[3].' '.$line[4];
+                            }
+                        }
+                         ?>
+                    </div>
                 </div>
                 <div class="data">
                     <div class="request">Дата рождения:</div>
-                    <div class="info">26.04.2003</div>
+                    <div class="info">
+                        <?php
+                        $login = $_SESSION['login'];
+                        $file_arr = file("users/data.csv");
+        				$lines = count($file_arr);
+                        for ($i = 0; $i < $lines; $i++) {
+                            $line = explode(';', $file_arr[$i]);
+        					if (strval($line[1]) == strval($login)) {
+                                echo $line[5];
+                            }
+                        }
+                         ?>
+                    </div>
                 </div>
                 <div class="data">
                     <div class="request">Дата оформления страховки:</div>
-                    <div class="info">02.03.2023</div>
+                    <div class="info">
+                        <?php
+                        $login = $_SESSION['login'];
+                        $file_arr = file("users/data.csv");
+        				$lines = count($file_arr);
+                        for ($i = 0; $i < $lines; $i++) {
+                            $line = explode(';', $file_arr[$i]);
+        					if (strval($line[1]) == strval($login)) {
+                                echo $line[6];
+                            }
+                        }
+                         ?>
+                    </div>
                 </div>
                 <div class="data">
                     <div class="request">Дата окончания страховки:</div>
-                    <div class="info">02.03.2024</div>
+                    <div class="info">
+                        <?php
+                        $login = $_SESSION['login'];
+                        $file_arr = file("users/data.csv");
+        				$lines = count($file_arr);
+                        for ($i = 0; $i < $lines; $i++) {
+                            $line = explode(';', $file_arr[$i]);
+        					if (strval($line[1]) == strval($login)) {
+                                echo $line[7];
+                            }
+                        }
+                         ?>
+                    </div>
                 </div>
                 <!-- <div class="data">
                     <div class="input__wrapper">
@@ -57,7 +105,7 @@
             </div>
         </div>
     </div>
-    <?php include 'footer.html'; ?>
+    <?php include 'footer.php'; ?>
 </body>
 
 </html>
