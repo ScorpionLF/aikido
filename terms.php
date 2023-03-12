73124eedaeded4dbad720381a7711f5585b39231
Liniza Khatipova <fanilovna03@gmail.com>
2023-03-12 19:50:38 +0300

    Fixed terms page

--- terms.php
+++ terms.php
@@ -22,185 +22,120 @@
     <?php include 'header.html'; ?>
     <div class="main">
         <div class="terms">
-            <div class="common">
-                <button onclick="showCommonElement('common_id')" class="terms_button">Общее</button>
-                <div id="common_id" class="terms-text">
-                    <!-- <p> -->
-                    <strong>Ай</strong> - Любовь, гармония. <br><br>
-                    <strong>Ки</strong> - жизненная энергия. <br><br>
-                    <strong>До</strong> - путь. <br><br>
-                    <strong>О-сэнсэй</strong> - Великий Учитель. <br><br>
-                    <strong>Сэнсэй </strong> - учитель. <br><br>
-                    <strong>Ути Дэси</strong> - ближайший ученик или последователь. <br><br>
-                    <strong>Сэмпай</strong> - старший ученик. <br><br>
-                    <strong>Кохай</strong> - младший ученик. <br><br>
-                    <strong>Aригато годзаимас</strong> - благодарю. <br><br>
-                    <strong>Xадзимэ</strong> - начинайте. <br><br>
-                    <strong>Ямэ</strong> - стойте. <br><br>
-                    <strong>Кирицу</strong> - встать. <br><br>
-                    <strong>Сэйдза</strong> - сесть. <br><br>
-                    <strong>Моксо (Мокусо)</strong> - мецитация, "молчание мыслей". <br><br>
-                    <strong>Рэй</strong> - поклон. <br><br>
-                    <strong>Дза рэй</strong> - поклон на коленях. <br><br>
-                    <strong>Рицу рэй</strong> - поклон стоя. <br><br>
-                    <strong>Гомэн насаи</strong> - прошу прощения. <br><br>
-                    <strong>Ии дэс</strong> - все в порядке, ничего страшного. <br><br>
-                    <strong>Кейко</strong> - тренировка. <br><br>
-                    <strong>Кимэ</strong> - концентрация силы. <br><br>
-                    <strong>Ки-но-нагарэ</strong> - поток ки. Энергия, подобная ручью. Работа в динамике.
-                    <br><br>
-                    <strong>Кю</strong> - ученическая степень. <br><br>
-                    <strong>Дан</strong> - мастерская степень. <br><br>
-                    <strong>Нагэ</strong> - проводящий прием партнер. <br><br>
-                    <strong>Укэ</strong> - атакующий партнер. <br><br>
-                    <strong>Кимоно</strong> - традиционная японская одежда. <br><br>
-                    <strong>Хакама</strong> - штаны-юбка черного цвета. <br><br>
-                    <strong>Оби</strong> - пояс. <br><br>
-                    <strong>Боккэн</strong> - деревянный меч для занятий. <br><br>
-                    <strong>Танто</strong> - деревянный нож для занятий. <br><br>
-                    <strong>Дзё</strong> - деревянная палка для занятий. <br><br>
-                    <strong>Додзё</strong> - место постижения пути, зал. <br><br>
-                    <strong>Татами</strong> - маты (исходно - японские соломенные). <br><br>
-                    <strong>Камидза</strong> - "верхнее место; почётное место", главное место в зале.
-                    <br><br>
-                    <strong>Шомэн</strong> - фасад, фронт, лицевая часть чего-либо. Также - название свитка с
-                    каллиграфией, который обычно расположен у камидза. <br><br>
-                    <!-- </p> -->
-                </div>
-                <script>
-                    function showCommonElement(common_id) {
-                        if (document.getElementById(common_id)) {
-                            var common = document.getElementById(common_id);
-                            if (common.style.display == "none") {
-                                common.style.display = "block";
-                            } else common.style.display = "none";
-                        } else console.log("WRONG!!!");
-                    }
-                </script>
-            </div>
-            <div class="numbers">
-                <button onclick="showNumberslement('numbers_id')" class="terms_button">Счет</button>
-                <div id="numbers_id" class="terms-text">
-                    <strong>Ити</strong> - Один <br><br>
-                    <strong>Ни</strong> - Два. <br><br>
-                    <strong>Сан</strong> - Три. <br><br>
-                    <strong>Си</strong> - Четыре. <br><br>
-                    <strong>Го</strong> - Пять. <br><br>
-                    <strong>Року</strong> - Шесть. <br><br>
-                    <strong>Сити</strong> - Семь. <br><br>
-                    <strong>Хати</strong> - Восемь. <br><br>
-                    <strong>Кю:</strong> - Девять. <br><br>
-                    <strong>Дзю</strong> - Десять. <br><br>
-                </div>
-                <script>
-                    function showNumberslement(numbers_id) {
-                        if (document.getElementById(numbers_id)) {
-                            var numbers = document.getElementById(numbers_id);
-                            if (numbers.style.display == "none") {
-                                numbers.style.display = "block";
-                            } else numbers.style.display = "none";
-                        } else console.log("WRONG!!!");
-                    }
-                </script>
-            </div>
+            <details>
+                <summary class="terms_button">Общее</summary>
+                <strong>Ай</strong> - Любовь, гармония. <br><br>
+                <strong>Ки</strong> - жизненная энергия. <br><br>
+                <strong>До</strong> - путь. <br><br>
+                <strong>О-сэнсэй</strong> - Великий Учитель. <br><br>
+                <strong>Сэнсэй </strong> - учитель. <br><br>
+                <strong>Ути Дэси</strong> - ближайший ученик или последователь. <br><br>
+                <strong>Сэмпай</strong> - старший ученик. <br><br>
+                <strong>Кохай</strong> - младший ученик. <br><br>
+                <strong>Aригато годзаимас</strong> - благодарю. <br><br>
+                <strong>Xадзимэ</strong> - начинайте. <br><br>
+                <strong>Ямэ</strong> - стойте. <br><br>
+                <strong>Кирицу</strong> - встать. <br><br>
+                <strong>Сэйдза</strong> - сесть. <br><br>
+                <strong>Моксо (Мокусо)</strong> - мецитация, "молчание мыслей". <br><br>
+                <strong>Рэй</strong> - поклон. <br><br>
+                <strong>Дза рэй</strong> - поклон на коленях. <br><br>
+                <strong>Рицу рэй</strong> - поклон стоя. <br><br>
+                <strong>Гомэн насаи</strong> - прошу прощения. <br><br>
+                <strong>Ии дэс</strong> - все в порядке, ничего страшного. <br><br>
+                <strong>Кейко</strong> - тренировка. <br><br>
+                <strong>Кимэ</strong> - концентрация силы. <br><br>
+                <strong>Ки-но-нагарэ</strong> - поток ки. Энергия, подобная ручью. Работа в динамике.
+                <br><br>
+                <strong>Кю</strong> - ученическая степень. <br><br>
+                <strong>Дан</strong> - мастерская степень. <br><br>
+                <strong>Нагэ</strong> - проводящий прием партнер. <br><br>
+                <strong>Укэ</strong> - атакующий партнер. <br><br>
+                <strong>Кимоно</strong> - традиционная японская одежда. <br><br>
+                <strong>Хакама</strong> - штаны-юбка черного цвета. <br><br>
+                <strong>Оби</strong> - пояс. <br><br>
+                <strong>Боккэн</strong> - деревянный меч для занятий. <br><br>
+                <strong>Танто</strong> - деревянный нож для занятий. <br><br>
+                <strong>Дзё</strong> - деревянная палка для занятий. <br><br>
+                <strong>Додзё</strong> - место постижения пути, зал. <br><br>
+                <strong>Татами</strong> - маты (исходно - японские соломенные). <br><br>
+                <strong>Камидза</strong> - "верхнее место; почётное место", главное место в зале.
+                <br><br>
+                <strong>Шомэн</strong> - фасад, фронт, лицевая часть чего-либо. Также - название свитка с
+                каллиграфией, который обычно расположен у камидза. <br><br>
+            </details>
+            <details>
+                <summary class="terms_button">Счет</summary>
+                <strong>Ити</strong> - Один <br><br>
+                <strong>Ни</strong> - Два. <br><br>
+                <strong>Сан</strong> - Три. <br><br>
+                <strong>Си</strong> - Четыре. <br><br>
+                <strong>Го</strong> - Пять. <br><br>
+                <strong>Року</strong> - Шесть. <br><br>
+                <strong>Сити</strong> - Семь. <br><br>
+                <strong>Хати</strong> - Восемь. <br><br>
+                <strong>Кю:</strong> - Девять. <br><br>
+                <strong>Дзю</strong> - Десять. <br><br>
+            </details>
+            <details>
+                <summary class="terms_button">Стороны и части тела</summary>
+                <strong>Те</strong> - рука. <br><br>
+                <strong>Тай</strong> - тело. <br><br>
+                <strong>Куби</strong> - горло. <br><br>
+                <strong>Тэкуби</strong> - предплечье. <br><br>
+                <strong>Мунэ</strong> - грудь. <br><br>
+                <strong>Ката</strong> - плечо. <br><br>
+                <strong>Аси</strong> - стопа, шаг. <br><br>
+                <strong>Эри</strong> - ворот. <br><br>
+                <strong>Хидзи</strong> - локоть. <br><br>
+                <strong>Котэ</strong> - запястье. <br><br>
+                <strong>Хара</strong> - живот. <br><br>
+                <strong>Коси</strong> - бедро, поясница. <br><br>
+                <strong>Удэ</strong> - локоть. <br><br>
+                <strong>Мэн</strong> - лицо. <br><br>
+                <strong>Тэ-гатана</strong> - рука-меч. <br><br>
+                <strong>Дану</strong> - ровень. <br><br>
+                <strong>Дзё</strong> - верх. <br><br>
+                <strong>Тю (Чу)</strong> - середина. <br><br>
+                <strong>Гэ (Ги)</strong> - низ. <br><br>
+                <strong>Миги</strong> - правый. <br><br>
+                <strong>Хидари</strong> - левый. <br><br>
+                <strong>Маэ</strong> - передний, вперёд. <br><br>
+                <strong>Усиро</strong> - задний, назад. <br><br>
+                <strong>Ёко</strong> - боковой, вбок. <br><br>
+            </details>
+            <details>
+                <summary class="terms_button">Стойки</summary>
+                <strong>Камаэ</strong> - основная стойка. <br><br>
+                <strong>Сидзэнтай</strong> - стойка естественного ожидания. <br><br>
+                <strong>Кибадати</strong> - "стойка всадника". <br><br>
+                <strong>Ай ханми</strong> - одноименная стойка двух партнеров. <br><br>
+                <strong>Гяку ханми</strong> - разноименная стойка двух партнеров. <br><br>
+                <strong>Ма ай</strong> - дистанция между партнерами, дистанция гармонии, "две руки", "два меча".
+                <br><br>
+            </details>
+            <details>
+                <summary class="terms_button">Удары</summary>
+                <strong>Цки (Цуки)</strong> - прямой удар. <br><br>
+                <strong>Ути</strong> - удар ребром ладони. <br><br>
+                <strong>Ёкомэн ути</strong> - боковой удар в голову или шею. <br><br>
+                <strong>Сёмен ути</strong> - удар сверху. <br><br>
+                <strong>Гэри (Гири)</strong> - удар ногой. <br><br>
+                <strong>Тори (Дори)</strong> - захват. <br><br>
+                <strong>Кататэ тори</strong> - захват за руку. <br><br>
+                <strong>Ай ханми кататэ тори</strong> - захват за одноименную руку. <br><br>
+                <strong>Гяку ханми кататэ тори</strong> - за разноименную). <br><br>
+                <strong>Рё тэ дори</strong> - захват двумя руками за две руки. <br><br>
+                <strong>Кататэ рё тэ дори (Моро те дори)</strong> - обеими руками за одну. <br><br>
+                <strong>Ката дори</strong> - захват за отворот кимоно (плечо). <br><br>
+                <strong>Рё ката дори</strong> - за оба плеча. <br><br>
+                <strong>Содэ дори</strong> - захват локтя. <br><br>
+                <strong>Усиро кататэ дори кубэ симэ</strong> - захват сзади за руку и удушение. <br><br>
+                <strong>Мунэ дори</strong> - за отворот кимоно. <br><br>
+                <strong>Рё мунэ дори</strong> - за отворот кимоно двумя руками. <br><br>
+                <strong>Ката дори мен учи</strong> - захват плеча с одновременным ударом в лицо. <br><br>
+            </details>
 
-
-            <div class="side">
-                <button onclick="showSidelement('side_id')" class="terms_button">Стороны и части тела</button>
-                <div id="side_id" class="terms-text">
-                    <strong>Те</strong> - рука. <br><br>
-                    <strong>Тай</strong> - тело. <br><br>
-                    <strong>Куби</strong> - горло. <br><br>
-                    <strong>Тэкуби</strong> - предплечье. <br><br>
-                    <strong>Мунэ</strong> - грудь. <br><br>
-                    <strong>Ката</strong> - плечо. <br><br>
-                    <strong>Аси</strong> - стопа, шаг. <br><br>
-                    <strong>Эри</strong> - ворот. <br><br>
-                    <strong>Хидзи</strong> - локоть. <br><br>
-                    <strong>Котэ</strong> - запястье. <br><br>
-                    <strong>Хара</strong> - живот. <br><br>
-                    <strong>Коси</strong> - бедро, поясница. <br><br>
-                    <strong>Удэ</strong> - локоть. <br><br>
-                    <strong>Мэн</strong> - лицо. <br><br>
-                    <strong>Тэ-гатана</strong> - рука-меч. <br><br>
-                    <strong>Дану</strong> - ровень. <br><br>
-                    <strong>Дзё</strong> - верх. <br><br>
-                    <strong>Тю (Чу)</strong> - середина. <br><br>
-                    <strong>Гэ (Ги)</strong> - низ. <br><br>
-                    <strong>Миги</strong> - правый. <br><br>
-                    <strong>Хидари</strong> - левый. <br><br>
-                    <strong>Маэ</strong> - передний, вперёд. <br><br>
-                    <strong>Усиро</strong> - задний, назад. <br><br>
-                    <strong>Ёко</strong> - боковой, вбок. <br><br>
-                </div>
-                <script>
-                    function showSidelement(side_id) {
-                        if (document.getElementById(side_id)) {
-                            var side = document.getElementById(side_id);
-                            if (side.style.display == "none") {
-                                side.style.display = "block";
-                            } else side.style.display = "none";
-                        } else console.log("WRONG!!!");
-                    }
-                </script>
-            </div>
-
-            <div onclick="showRackElement('rack_id')" class="rack">
-                <button class="terms_button">Стойки</button>
-                <div id="rack_id" class="terms-text">
-                    <strong>Камаэ</strong> - основная стойка. <br><br>
-                    <strong>Сидзэнтай</strong> - стойка естественного ожидания. <br><br>
-                    <strong>Кибадати</strong> - "стойка всадника". <br><br>
-                    <strong>Ай ханми</strong> - одноименная стойка двух партнеров. <br><br>
-                    <strong>Гяку ханми</strong> - разноименная стойка двух партнеров. <br><br>
-                    <strong>Ма ай</strong> - дистанция между партнерами, дистанция гармонии, "две руки", "два меча".
-                    <br><br>
-                </div>
-                <script>
-                    function showRackElement(rack_id) {
-                        if (document.getElementById(rack_id)) {
-                            var rack = document.getElementById(rack_id);
-                            if (rack.style.display == "none") {
-                                rack.style.display = "block";
-                            } else rack.style.display = "none";
-                        } else console.log("WRONG!!!");
-                    }
-                </script>
-            </div>
-
-            <div onclick="showAttacksElement('attacks_id')" class="attacks">
-                <button class="terms_button">Удары</button>
-                <div id="attacks_id" class="terms-text">
-                    <strong>Цки (Цуки)</strong> - прямой удар. <br><br>
-                    <strong>Ути</strong> - удар ребром ладони. <br><br>
-                    <strong>Ёкомэн ути</strong> - боковой удар в голову или шею. <br><br>
-                    <strong>Сёмен ути</strong> - удар сверху. <br><br>
-                    <strong>Гэри (Гири)</strong> - удар ногой. <br><br>
-                    <strong>Тори (Дори)</strong> - захват. <br><br>
-                    <strong>Кататэ тори</strong> - захват за руку. <br><br>
-                    <strong>Ай ханми кататэ тори</strong> - захват за одноименную руку. <br><br>
-                    <strong>Гяку ханми кататэ тори</strong> - за разноименную). <br><br>
-                    <strong>Рё тэ дори</strong> - захват двумя руками за две руки. <br><br>
-                    <strong>Кататэ рё тэ дори (Моро те дори)</strong> - обеими руками за одну. <br><br>
-                    <strong>Ката дори</strong> - захват за отворот кимоно (плечо). <br><br>
-                    <strong>Рё ката дори</strong> - за оба плеча. <br><br>
-                    <strong>Содэ дори</strong> - захват локтя. <br><br>
-                    <strong>Усиро кататэ дори кубэ симэ</strong> - захват сзади за руку и удушение. <br><br>
-                    <strong>Мунэ дори</strong> - за отворот кимоно. <br><br>
-                    <strong>Рё мунэ дори</strong> - за отворот кимоно двумя руками. <br><br>
-                    <strong>Ката дори мен учи</strong> - захват плеча с одновременным ударом в лицо. <br><br>
-                </div>
-                <script>
-                    function showAttacksElement(attacks_id) {
-                        if (document.getElementById(attacks_id)) {
-                            var attacks = document.getElementById(attacks_id);
-                            if (attacks.style.display == "none") {
-                                attacks.style.display = "block";
-                            } else attacks.style.display = "none";
-                        } else console.log("WRONG!!!");
-                    }
-                </script>
-            </div>
         </div>
     </div>
     <?php include 'footer.php'; ?>
