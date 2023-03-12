8df3317fddd2df4b3250aaa13976f4f2eb180a52
Liniza Khatipova <fanilovna03@gmail.com>
2023-03-12 20:29:21 +0300

    Created technique page

--- technique.php
+++ technique.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@200&family=Inter&family=Roboto&display=swap"
         rel="stylesheet">
-    <title>Инструкторы</title>
+    <title>Техники</title>
 </head>
 
 <body>
@@ -22,7 +22,88 @@
 
     <div class="main">
         <div class="techique">
-            <div style="text-align: center;">В разработке</div>
+            <details>
+                <summary class="terms_button">Падения / Страховки</summary>
+                <div class="tech_block">
+                    <div id="tech_block_left">
+                        <div class="video_block">
+                            <img class="video" src="images/image.jpg" alt="video">
+                            <div class="video_title">Маэ укэми (Сувари Вадза)</div>
+                        </div>
+                    </div>
+                    <div id="tech_block_center">
+                        <div class="video_block">
+                            <img class="video" src="images/image.jpg" alt="video">
+                            <div class="video_title">Маэ укэми (Сувари Вадза)</div>
+                        </div>
+                    </div>
+                    <div id="tech_block_right">
+                        <div class="video_block">
+                            <img class="video" src="images/image.jpg" alt="video">
+                            <div class="video_title">Маэ укэми (Сувари Вадза)</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tech_block">
+                    <div id="tech_block_left">
+                        <div class="video_block">
+                            <img class="video" src="images/image.jpg" alt="video">
+                            <div class="video_title">Маэ укэми (Сувари Вадза)</div>
+                        </div>
+                    </div>
+                    <div id="tech_block_center">
+                        <div class="video_block">
+                            <img class="video" src="images/image.jpg" alt="video">
+                            <div class="video_title">Маэ укэми (Сувари Вадза)</div>
+                        </div>
+                    </div>
+                    <div id="tech_block_right">
+                        <div class="video_block">
+                            <img class="video" src="images/image.jpg" alt="video">
+                            <div class="video_title">Маэ укэми (Сувари Вадза)</div>
+                        </div>
+                    </div>
+                </div>
+            </details>
+            <details>
+                <summary class="terms_button">Перемещения</summary>
+
+            </details>
+            <details>
+                <summary class="terms_button">Боевые контроли</summary>
+
+            </details>
+            <details>
+                <summary class="terms_button">Приемы</summary>
+
+            </details>
+            <details>
+                <summary class="terms_button">Экзамен</summary>
+                <details>
+                    <summary class="exam_button">5 кю</summary>
+
+                </details>
+                <details>
+                    <summary class="exam_button">6 кю</summary>
+
+                </details>
+                <details>
+                    <summary class="exam_button">7 кю</summary>
+
+                </details>
+                <details>
+                    <summary class="exam_button">8 кю</summary>
+
+                </details>
+                <details>
+                    <summary class="exam_button">9 кю</summary>
+
+                </details>
+                <details>
+                    <summary class="exam_button">10 кю</summary>
+
+                </details>
+            </details>
         </div>
     </div>
 
