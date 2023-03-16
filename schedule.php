ee0647196b5c76905faaeff24914ac1bcb9422f5
Liniza Khatipova <fanilovna03@gmail.com>
2023-03-16 23:46:02 +0300

    Change Schtdule

--- schedule.php
+++ schedule.php
@@ -25,41 +25,66 @@
         <div class="schedule">
             <div class="schedule_left_div">
                 <div class="table_info">
-                    <div class="adress">Ул. Ямашева, д. 36 <br>Белоусов Е.Н.</div>
+                    Дети
+                    <!-- <div class="adress">Ул. Ямашева, д. 36 <br>Белоусов Е.Н.</div> -->
                 </div>
                 <div class="table">
                     <table>
                         <tr>
-                            <th>Дни недели</th>
-                            <th>Детская группа</th>
-                            <th>Взрослая группа</th>
+                            <th>Дни</th>
+                            <th>Время</th>
+                            <th>Тренер</th>
+                            <th>Адрес</th>
                         </tr>
                         <tr>
-                            <th>Понедельник</th>
+                            <th>Понедельник, Среда, Пятница</th>
                             <th>18.00 - 19.00</th>
-                            <th>19.00 - 21.00</th>
+                            <th>Белоусов Е.Н.</th>
+                            <th>ул. Ямашева, д. 36</th>
                         </tr>
                         <tr>
-                            <th>Среда</th>
+                            <th>Понедельник, Среда, Пятница</th>
                             <th>18.00 - 19.00</th>
-                            <th>19.00 - 21.00</th>
+                            <th>Белоусов Е.Н.</th>
+                            <th>ул. Ямашева, д. 36</th>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table_info">
+                    Взрослые
+                    <!-- <div class="adress">Ул. Ямашева, д. 36 <br>Белоусов Е.Н.</div> -->
+                </div>
+                <div class="table">
+                    <table>
+                        <tr>
+                            <th>Дни</th>
+                            <th>Время</th>
+                            <th>Тренер</th>
+                            <th>Адрес</th>
                         </tr>
                         <tr>
-                            <th>Пятница</th>
+                            <th>Понедельник, Среда, Пятница</th>
                             <th>18.00 - 19.00</th>
-                            <th>19.00 - 21.00</th>
+                            <th>Белоусов Е.Н.</th>
+                            <th>ул. Ямашева, д. 36</th>
+                        </tr>
+                        <tr>
+                            <th>Понедельник, Среда, Пятница</th>
+                            <th>18.00 - 19.00</th>
+                            <th>Белоусов Е.Н.</th>
+                            <th>ул. Ямашева, д. 36</th>
                         </tr>
                     </table>
                 </div>
             </div>
-            <div class="schedule_right_div">
+            <!-- <div class="schedule_right_div">
                 <div class="maps">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1120.4904545474737!2d49.10169281735697!3d55.8282906571291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415952ae457ec33f%3A0x942bb741ea0d311!2z0L_RgC3Rgi4g0KXRg9GB0LDQuNC90LAg0K_QvNCw0YjQtdCy0LAsIDM2LCDQmtCw0LfQsNC90YwsINCg0LXRgdC_LiDQotCw0YLQsNGA0YHRgtCw0L0sIDQyMDA0NA!5e0!3m2!1sru!2sru!4v1675417328707!5m2!1sru!2sru"
                         width="630" height="400" style="border:0;" allowfullscreen="" loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-            </div>
+            </div> -->
         </div>
 
     </div>
