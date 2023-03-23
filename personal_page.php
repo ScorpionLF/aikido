acb9e533dbfe35c6cbdfa866bdea46bd5d640e05
Dasha <ideliya3004@gmail.com>
2023-03-23 23:52:32 +0300

    Some changes in personal_page

--- personal_page.php
+++ personal_page.php
@@ -1,5 +1,9 @@
 <?php session_start();?>
-
+<?php
+if ($_SESSION['login'] == 'admin') {
+    header('Location:admin_page.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="ru">
 
@@ -23,29 +27,37 @@
 
 <body>
     <?php include 'header.html'; ?>
+
+    <?php
+    $login = $_SESSION['login'];
+    $file_arr = file("users/users.csv");
+    $lines = count($file_arr);
+    for ($i = 0; $i < $lines; $i++) {
+        $line = explode(';', $file_arr[$i]);
+        if (strval($line[1]) == strval($login)) {
+            $id = strval($line[0]);
+        }
+    }
+
+    $file_arr = file("users/data.csv");
+    $lines = count($file_arr);
+    for ($i = 0; $i < $lines; $i++) {
+        $line = explode(';', $file_arr[$i]);
+        if (strval($line[0]) == $id) {
+    $certificate_src = "users/".strval($id).".jpg";
+    ?>
+
     <div class="personal_page">
-        <div class="i_title_div">
-            <div class="title_page_i">Личные данные</div>
-            <div class="yellow_line"></div>
-        </div>
         <div class="block_personal_page">
             <div class="photo_personal">
-                <img class="img_person" src="images/image.jpg" alt="">
+                <img class="img_person" src=<?php echo $certificate_src  ?> alt="">
             </div>
             <div class="block_person">
                 <div class="fio">
                     <div class="request">ФИО:</div>
                     <div class="info">
                         <?php
-                        $login = $_SESSION['login'];
-                        $file_arr = file("users/data.csv");
-        				$lines = count($file_arr);
-                        for ($i = 0; $i < $lines; $i++) {
-                            $line = explode(';', $file_arr[$i]);
-        					if (strval($line[1]) == strval($login)) {
-                                echo $line[2].' '.$line[3].' '.$line[4];
-                            }
-                        }
+                                echo $line[1].' '.$line[2].' '.$line[3];
                          ?>
                     </div>
                 </div>
@@ -53,48 +65,29 @@
                     <div class="request">Дата рождения:</div>
                     <div class="info">
                         <?php
-                        $login = $_SESSION['login'];
-                        $file_arr = file("users/data.csv");
-        				$lines = count($file_arr);
-                        for ($i = 0; $i < $lines; $i++) {
-                            $line = explode(';', $file_arr[$i]);
-        					if (strval($line[1]) == strval($login)) {
-                                echo $line[5];
-                            }
-                        }
+                                echo $line[4];
                          ?>
                     </div>
                 </div>
                 <div class="data">
-                    <div class="request">Дата оформления страховки:</div>
+                    <div class="request"><br>Срок действия страхования:</div>
+                </div>
+                <div class="data">
+                    <div class="request">&nbsp;&nbsp;Дата начала:</div>
                     <div class="info">
                         <?php
-                        $login = $_SESSION['login'];
-                        $file_arr = file("users/data.csv");
-        				$lines = count($file_arr);
-                        for ($i = 0; $i < $lines; $i++) {
-                            $line = explode(';', $file_arr[$i]);
-        					if (strval($line[1]) == strval($login)) {
+                                echo $line[5];
+                         ?>
+                    </div>
+                </div>
+                <div class="data">
+                    <div class="request">&nbsp;&nbsp;Дата окончания:</div>
+                    <div class="info">
+                        <?php
                                 echo $line[6];
                             }
                         }
-                         ?>
-                    </div>
-                </div>
-                <div class="data">
-                    <div class="request">Дата окончания страховки:</div>
-                    <div class="info">
-                        <?php
-                        $login = $_SESSION['login'];
-                        $file_arr = file("users/data.csv");
-        				$lines = count($file_arr);
-                        for ($i = 0; $i < $lines; $i++) {
-                            $line = explode(';', $file_arr[$i]);
-        					if (strval($line[1]) == strval($login)) {
-                                echo $line[7];
-                            }
-                        }
-                         ?>
+                        ?>
                     </div>
                 </div>
                 <!-- <div class="data">
