190e486763e7ec6c58ec2c9beff0fcf6c50ac977
Liniza Khatipova <fanilovna03@gmail.com>
2023-03-23 21:33:18 +0300

    The first version of the page Contacts

--- contacts.php
+++ contacts.php
@@ -13,17 +13,68 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@200&family=Inter&family=Roboto&display=swap"
         rel="stylesheet">
-    <title>Расписание</title>
+    <title>Контакты</title>
 </head>
 
 <body>
     <?php include 'header.html'; ?>
 
     <div class="main">
+        <!-- <div class="maps" style="width: 100%; margin-top: -50px;">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1120.4904545474737!2d49.10169281735697!3d55.8282906571291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415952ae457ec33f%3A0x942bb741ea0d311!2z0L_RgC3Rgi4g0KXRg9GB0LDQuNC90LAg0K_QvNCw0YjQtdCy0LAsIDM2LCDQmtCw0LfQsNC90YwsINCg0LXRgdC_LiDQotCw0YLQsNGA0YHRgtCw0L0sIDQyMDA0NA!5e0!3m2!1sru!2sru!4v1675417328707!5m2!1sru!2sru"
+                style="border: none; height: 300px; width: 100%;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
         <div class="contact_container">
-            В разработке
+            <div class="left_div">
+                <div class="title_contact">Адрес</div>
+                <div class="info_contact">Ул. Ямашева, д. 36</div>
+
+            </div>
+            <div class="right_div">
+                <div class="title_contact">Контакты</div>
+                <div class="info_contact"><strong>Телефон:</strong> +7 (999) 999-99-99</div>
+                <div class="info_contact"><strong>Вконтакте:</strong> ссылка</div>
+                <div class="info_contact"><strong>YouTube:</strong> ссылка</div>
+            </div> -->
+
+        <div class="contact_container">
+            <div class="left_div">
+                <div class="info_contact"><strong>Адрес:</strong> Ул. Ямашева, д. 36</div>
+                <div class="info_contact"><strong>Телефон:</strong> +7 (999) 999-99-99</div>
+                <div class="info_contact"><strong>Вконтакте:</strong> ссылка</div>
+                <div class="info_contact"><strong>YouTube:</strong> ссылка</div>
+            </div>
+            <div class="right_div">
+                <div class="maps" style="width: 100%; ">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1120.4904545474737!2d49.10169281735697!3d55.8282906571291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415952ae457ec33f%3A0x942bb741ea0d311!2z0L_RgC3Rgi4g0KXRg9GB0LDQuNC90LAg0K_QvNCw0YjQtdCy0LAsIDM2LCDQmtCw0LfQsNC90YwsINCg0LXRgdC_LiDQotCw0YLQsNGA0YHRgtCw0L0sIDQyMDA0NA!5e0!3m2!1sru!2sru!4v1675417328707!5m2!1sru!2sru"
+                        style="border: none; height: 300px; width: 100%;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
         </div>
 
+        <!-- <div class="contact_container">
+            <div class="first">
+                <div class="left_div">
+                    <div class="info_contact"><strong>Адрес:</strong> Ул. Ямашева, д. 36</div>
+                    <div class="info_contact"><strong>Вконтакте:</strong> ссылка</div>
+                </div>
+                <div class="right_div">
+                    <div class="info_contact"><strong>Телефон:</strong> +7 (999) 999-99-99</div>
+                    <div class="info_contact"><strong>YouTube:</strong> ссылка</div>
+                </div>
+            </div>
+            <div class="maps" style="width: 100%; margin-bottom: -50px;">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1120.4904545474737!2d49.10169281735697!3d55.8282906571291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415952ae457ec33f%3A0x942bb741ea0d311!2z0L_RgC3Rgi4g0KXRg9GB0LDQuNC90LAg0K_QvNCw0YjQtdCy0LAsIDM2LCDQmtCw0LfQsNC90YwsINCg0LXRgdC_LiDQotCw0YLQsNGA0YHRgtCw0L0sIDQyMDA0NA!5e0!3m2!1sru!2sru!4v1675417328707!5m2!1sru!2sru"
+                    style="border: none; height: 300px; width: 100%;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div> -->
+    </div>
     </div>
 
     <?php include 'footer.php'; ?>
