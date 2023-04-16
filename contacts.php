03e90d440d4434654645373e14750590141254a0
Liniza Khatipova <fanilovna03@gmail.com>
2023-04-16 22:16:54 +0300

    Contacts: removed all extra lines in the style file, changed the names of the classes

--- contacts.php
+++ contacts.php
@@ -14,85 +14,31 @@
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@200&family=Inter&family=Roboto&display=swap"
         rel="stylesheet">
     <title>Контакты</title>
-    <link type="image/x-icon" href="images/favicon.png" rel="shortcut icon">
-    <link type="Image/x-icon" href="images/favicon.png" rel="icon"></head>
+    <link type="Image/x-icon" href="images/favicon.png" rel="icon">
+</head>
 </head>
 
 <body>
     <?php include 'header.html'; ?>
-
     <div class="main">
-        <!-- <div class="maps" style="width: 100%; margin-top: -50px;">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1120.4904545474737!2d49.10169281735697!3d55.8282906571291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415952ae457ec33f%3A0x942bb741ea0d311!2z0L_RgC3Rgi4g0KXRg9GB0LDQuNC90LAg0K_QvNCw0YjQtdCy0LAsIDM2LCDQmtCw0LfQsNC90YwsINCg0LXRgdC_LiDQotCw0YLQsNGA0YHRgtCw0L0sIDQyMDA0NA!5e0!3m2!1sru!2sru!4v1675417328707!5m2!1sru!2sru"
-                style="border: none; height: 300px; width: 100%;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-        <div class="contact_container">
-            <div class="left_div">
-                <div class="title_contact">Адрес</div>
-                <div class="info_contact">Ул. Ямашева, д. 36</div>
-
+        <section class="contacts">
+            <div class="block-contacts-informations">
+                <div class="contacts-information-line"><strong>Адрес:</strong> ул. Ямашева, д. 36</div>
+                <div class="contacts-information-line"><strong>Телефон:</strong> +7 (999) 999-99-99</div>
+                <div class="contacts-information-line"><strong>Вконтакте:</strong> ссылка</div>
+                <div class="contacts-information-line"><strong>YouTube:</strong> ссылка</div>
             </div>
-            <div class="right_div">
-                <div class="title_contact">Контакты</div>
-                <div class="info_contact"><strong>Телефон:</strong> +7 (999) 999-99-99</div>
-                <div class="info_contact"><strong>Вконтакте:</strong> ссылка</div>
-                <div class="info_contact"><strong>YouTube:</strong> ссылка</div>
-            </div> -->
-
-        <div class="contact_container">
-            <div class="left_div">
-                <div class="info_contact"><strong>Адрес:</strong> ул. Ямашева, д. 36</div>
-                <div class="info_contact"><strong>Телефон:</strong> +7 (999) 999-99-99</div>
-                <div class="info_contact"><strong>Вконтакте:</strong> ссылка</div>
-                <div class="info_contact"><strong>YouTube:</strong> ссылка</div>
-            </div>
-            <div class="right_div">
-                <img class="img_map" src="images/screen_map.png"  alt="">
-                <div class="maps">
-                    <!-- <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1120.4904545474737!2d49.10169281735697!3d55.8282906571291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415952ae457ec33f%3A0x942bb741ea0d311!2z0L_RgC3Rgi4g0KXRg9GB0LDQuNC90LAg0K_QvNCw0YjQtdCy0LAsIDM2LCDQmtCw0LfQsNC90YwsINCg0LXRgdC_LiDQotCw0YLQsNGA0YHRgtCw0L0sIDQyMDA0NA!5e0!3m2!1sru!2sru!4v1675417328707!5m2!1sru!2sru"
-                        style="border: none; height: 300px; width: 100%;" allowfullscreen="" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+            <div class="block-contacts-map">
+                <img class="contacts-img-map" src="images/screen_map.png" alt="">
+                <div class="contacts-map">
                     <iframe
                         src="https://yandex.ru/map-widget/v1/?ll=49.101661%2C55.829023&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NjI2Mzc4NBJo0KDQvtGB0YHQuNGPLCDQoNC10YHQv9GD0LHQu9C40LrQsCDQotCw0YLQsNGA0YHRgtCw0L0sINCa0LDQt9Cw0L3RjCwg0L_RgNC-0YHQv9C10LrRgiDQr9C80LDRiNC10LLQsCwgMzYiCg08aERCFbNQX0I%2C&z=16.61"
                         height="400" frameborder="1" allowfullscreen="true"
                         style="position:relative; border: none; height: 330px; width: 100%;"></iframe>
                 </div>
-                <!-- <div style="position:relative;overflow:hidden;"><a
-                        href="https://yandex.ru/maps/43/kazan/?utm_medium=mapframe&utm_source=maps"
-                        style="color:#eee;font-size:12px;position:absolute;top:0px;">Казань</a><a
-                        href="https://yandex.ru/maps/43/kazan/house/prospekt_yamasheva_36/YEAYdwdhTk0PQFtvfXRzeXRjbQ==/?ll=49.101661%2C55.829023&utm_medium=mapframe&utm_source=maps&z=16.61"
-                        style="color:#eee;font-size:12px;position:absolute;top:14px;">Проспект Ямашева, 36 —
-                        Яндекс Карты</a><iframe
-                        src="https://yandex.ru/map-widget/v1/?ll=49.101661%2C55.829023&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NjI2Mzc4NBJo0KDQvtGB0YHQuNGPLCDQoNC10YHQv9GD0LHQu9C40LrQsCDQotCw0YLQsNGA0YHRgtCw0L0sINCa0LDQt9Cw0L3RjCwg0L_RgNC-0YHQv9C10LrRgiDQr9C80LDRiNC10LLQsCwgMzYiCg08aERCFbNQX0I%2C&z=16.61"
-                        width="560" height="400" frameborder="1" allowfullscreen="true"
-                        style="position:relative;"></iframe></div>
-            </div> -->
             </div>
-
-            <!-- <div class="contact_container">
-            <div class="first">
-                <div class="left_div">
-                    <div class="info_contact"><strong>Адрес:</strong> Ул. Ямашева, д. 36</div>
-                    <div class="info_contact"><strong>Вконтакте:</strong> ссылка</div>
-                </div>
-                <div class="right_div">
-                    <div class="info_contact"><strong>Телефон:</strong> +7 (999) 999-99-99</div>
-                    <div class="info_contact"><strong>YouTube:</strong> ссылка</div>
-                </div>
-            </div>
-            <div class="maps" style="width: 100%; margin-bottom: -50px;">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1120.4904545474737!2d49.10169281735697!3d55.8282906571291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415952ae457ec33f%3A0x942bb741ea0d311!2z0L_RgC3Rgi4g0KXRg9GB0LDQuNC90LAg0K_QvNCw0YjQtdCy0LAsIDM2LCDQmtCw0LfQsNC90YwsINCg0LXRgdC_LiDQotCw0YLQsNGA0YHRgtCw0L0sIDQyMDA0NA!5e0!3m2!1sru!2sru!4v1675417328707!5m2!1sru!2sru"
-                    style="border: none; height: 300px; width: 100%;" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-        </div> -->
-        </div>
+        </section>
     </div>
-
     <?php include 'footer.php'; ?>
 </body>
 
