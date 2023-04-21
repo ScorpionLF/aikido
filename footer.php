d6e6800d3c06b333d706cb945a7e1caf6dceba1f
Liniza Khatipova <fanilovna03@gmail.com>
2023-04-21 10:12:19 +0300

    Removed duplicate styles

--- footer.php
+++ footer.php
@@ -5,9 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/footer_style.css">
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@200&family=Inter&family=Roboto&display=swap"
@@ -41,7 +39,7 @@
                         ?>
                         <a href="#open" class="block-footer-buttons-element-button">Вход</a>
                         <div id="open" class="block-footer-modalWindow">
-                            <a href="#" class="block-footer-modalWindow-overlay"></a>
+                            <a href="index" class="block-footer-modalWindow-overlay"></a>
                             <div class="block-footer-modalWindow-dialog">
                                 <div class="block-footer-modalWindow-dialog-header">
                                     <div class="block-footer-modalWindow-dialog-header-imgs">
--- personal_page.php
+++ personal_page.php
@@ -12,8 +12,6 @@ if ($_SESSION['login'] == 'admin') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/header_style.css">
-    <link rel="stylesheet" href="css/footer_style.css">
     <link rel="stylesheet" href="css/personal_page.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
