b562c77243486ae10738a495179690bd6c49c316
Liniza <fanilovna03@gmail.com>
2023-08-16 19:06:42 +0300

    Added styles for revealing/hiding news.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/news.css">
     <link rel="stylesheet" href="css/header_style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -23,89 +24,107 @@
         echo $header;
     ?>
     <main>
-            <section class="index-composition">
-                <div class="block-composition-left">
-                    <img class="block-composition-left-img" src="images/sensei.png" alt="composition-img-1">
-                </div>
-                <div class="block-composition-center">
-                    <div>
-                        <div class="block-composition-center-line"></div>
-                        <div class="block-composition-center-text">Настоящая победа - это победа над самим собой.</div>
-                        <div class="block-composition-center-line"></div>
-                    </div>
-                </div>
-                <div class="block-composition-right">
-                    <img class="block-composition-right-img" src="images/aikido_logo.png" alt="composition-img-2">
-                </div>
-            </section>
-            <section class="index-information">
-            <div class="mobile-title">
-                <div class="title-block">
-                    <div class="title">О Федерации</div>
-                    <div class="title-block-line"></div>
+        <section class="index-composition">
+            <div class="block-composition-left">
+                <img class="block-composition-left-img" src="images/sensei.png" alt="composition-img-1">
+            </div>
+            <div class="block-composition-center">
+                <div>
+                    <div class="block-composition-center-line"></div>
+                    <div class="block-composition-center-text">Настоящая победа - это победа над самим собой.</div>
+                    <div class="block-composition-center-line"></div>
                 </div>
             </div>
-                <div class="index-information-title">О Федерации</div>
-                <div class="index-information-text">
-                    <p>Федерация Айкидо Ametsuchi Dojo входит во Всестилевую Федерацию Айкидо России.
-                        Поэтому все ученики проходят ежегодную аттестацию в Москве или Санкт-Петербурге. Является
-                        организатором
-                        крупных международных фестивалей Айкидо в Казани, с участием ведущих сенсеев России и зарубежья.
-                        Каждый
-                        ученик проходит все мастер-классы данных фестивалей.</p>
-                    <p>Обучая вас самозащите, мы не воспитываем
-                        агрессию,
-                        не культивируем идею насилия в ответ насилию. Это позволяет сохранять спокойствие и уверенность в
-                        любой
-                        ситуации,
-                        в духе наследия Великого Мастера Уэсиба Морихей, который создал это единоборство после долгих лет
-                        совершенствования тела и духа.</p>
+            <div class="block-composition-right">
+                <img class="block-composition-right-img" src="images/aikido_logo.png" alt="composition-img-2">
+            </div>
+        </section>
+        <section class="index-information">
+        <div class="mobile-title">
+            <div class="title-block">
+                <div class="title">О Федерации</div>
+                <div class="title-block-line"></div>
+            </div>
+        </div>
+            <div class="index-information-title">О Федерации</div>
+            <div class="index-information-text">
+                <p>Федерация Айкидо Ametsuchi Dojo входит во Всестилевую Федерацию Айкидо России.
+                    Поэтому все ученики проходят ежегодную аттестацию в Москве или Санкт-Петербурге. Является
+                    организатором
+                    крупных международных фестивалей Айкидо в Казани, с участием ведущих сенсеев России и зарубежья.
+                    Каждый
+                    ученик проходит все мастер-классы данных фестивалей.</p>
+                <p>Обучая вас самозащите, мы не воспитываем
+                    агрессию,
+                    не культивируем идею насилия в ответ насилию. Это позволяет сохранять спокойствие и уверенность в
+                    любой
+                    ситуации,
+                    в духе наследия Великого Мастера Уэсиба Морихей, который создал это единоборство после долгих лет
+                    совершенствования тела и духа.</p>
+            </div>
+        </section>
+        <section class="index-news">
+            <a class="block-index-news" onclick="document.getElementById('open_news_1').style.display='block'"> 
+                <img class="block-news-img" src="images/new_6.jpg" onError="this.src='images/not_found.png'">
+                <div class="block-news-title">Открываем новый сезон тренировок!</div>
+                <div class="block-news-news">
+                <?php echo (file_get_contents('./all_news/new_6.txt')); ?>
+                    <div class="block-news-date">09.08.2023</div>
                 </div>
-            </section>
-            <section class="index-news">
-                <a href="news_page" style="text-decoration: none;">
-                    <div class="block-index-news">
-                        <img class="block-index-news-img" src="images/new_6.jpg" onError="this.src='images/not_found.png'">
-                        <div class="block-index-news-title">Открываем новый сезон тренировок!</div>
-                        <div class="block-index-news-news">
-                            Федерация Айкидо Ametsuchi Dojo объявляет набор в детские группы.
-                        </div>
-                        <input class="block__hidden" type="checkbox" id="is_hidden">
-                        <label for="is_hidden">Прочитать всё.</label>
-                        <div class="hidden__news">
+            </a>  
+            <div id="open_news_1" class="modal">
+                <span onclick="document.getElementById('open_news_1').style.display='none'" class="close" title="Close Modal">&times;</span>     
+                <div class="modal-news-content">
+                    <img class="modal__img" src="images/new_6.jpg" onError="this.src='images/not_found.png'">
+                    <div class="block__news">
+                        <div class="block-news-title">Открываем новый сезон тренировок!</div>
+                        <p class="modal__news">
                             <?php echo (file_get_contents('./all_news/new_6.txt')); ?>
-                        </div>
+                        </p>
                     </div>
-                </a>
-                <a href="news_page" style="text-decoration: none;">
-                    <div class="block-index-news">
-                        <img class="block-index-news-img" src="images/new_5.jpg" onError="this.src='images/not_found.png'">
-                        <div class="block-index-news-title">Ученики Ametsuchi отдыхают в Анапе</div>
-                        <div class="block-index-news-news">
-                            Ученики Ametsuchi отдыхают в Анапе на берегу черного моря.
-                        </div>
-                        <input class="block__hidden" type="checkbox" id="is_hidden_2">
-                        <label for="is_hidden_2">Прочитать всё.</label>
-                        <div class="hidden__news">
+                </div> 
+            </div> 
+            <a class="block-index-news" onclick="document.getElementById('open_news_2').style.display='block'"> 
+                <img class="block-news-img" src="images/new_5.jpg" onError="this.src='images/not_found.png'">
+                <div class="block-news-title">Ученики Ametsuchi отдыхают в Анапе</div>
+                <div class="block-news-news">
+                <?php echo (file_get_contents('./all_news/new_5.txt')); ?>
+                    <div class="block-news-date">18.07.2022</div>
+                </div>
+            </a>  
+            <div id="open_news_2" class="modal">
+                <span onclick="document.getElementById('open_news_2').style.display='none'" class="close" title="Close Modal">&times;</span>     
+                <div class="modal-news-content">
+                    <img class="modal__img" src="images/new_5.jpg" onError="this.src='images/not_found.png'">
+                    <div class="block__news">
+                        <div class="block-news-title">Ученики Ametsuchi отдыхают в Анапе</div>
+                        <p class="modal__news">
                             <?php echo (file_get_contents('./all_news/new_5.txt')); ?>
-                        </div>
+                        </p>
                     </div>
-                </a>
-                <a href="news_page" style="text-decoration: none;">
-                    <div class="block-index-news">
-                        <img class="block-index-news-img" src="images/new_2.jpg" onError="this.src='images/not_found.png'">
-                        <div class="block-index-news-title">Семинар Сенсея Халидова!</div>
-                        <div class="block-index-news-news">
-                            Прошел 4-й международный семинар Айкидо под руководством Сенсея Халидова!
-                        </div>
-                        <input class="block__hidden" type="checkbox" id="is_hidden_3">
-                        <label for="is_hidden_3">Прочитать всё.</label>
-                        <div class="hidden__news">
-                            <?php echo (file_get_contents('./all_news/new_2.txt')); ?>
-                        </div>
+                </div> 
+            </div> 
+            <a class="block-index-news" onclick="document.getElementById('open_news_3').style.display='block'"> 
+                <img class="block-news-img" src="images/new_4.jpg" onError="this.src='images/not_found.png'">
+                <div class="block-news-title">Ametsuchi посетил Сенсей Сергей Флоренко</div>
+                <div class="block-news-news">
+                <?php echo (file_get_contents('./all_news/new_3.txt')); ?>
+                    <div class="block-news-date">22.06.2023</div>
+                </div>
+            </a>  
+            <div id="open_news_3" class="modal">
+                <span onclick="document.getElementById('open_news_3').style.display='none'" class="close" title="Close Modal">&times;</span>     
+                <div class="modal-news-content">
+                    <img class="modal__img" src="images/new_4.jpg" onError="this.src='images/not_found.png'">
+                    <div class="block__news">
+                        <div class="block-news-title">Ametsuchi посетил Сенсей Сергей Флоренко</div>
+                        <p class="modal__news">
+                            <?php echo (file_get_contents('./all_news/new_3.txt')); ?>
+                        </p>
                     </div>
-                </a>
-            </section>
+                </div> 
+            </div> 
+        </section>
     </main>
     <?php include 'footer.php'; ?>
 
--- news_page.php
+++ news_page.php
@@ -40,21 +40,12 @@
                     <img class="modal__img" src="images/new_6.jpg" onError="this.src='images/not_found.png'">
                     <div class="block__news">
                         <div class="block-news-title">Открываем новый сезон тренировок!</div>
-                        <div class="modal__news">
+                        <p class="modal__news">
                             <?php echo (file_get_contents('./all_news/new_6.txt')); ?>
-                        </div>
+                        </p>
                     </div>
                 </div> 
             </div> 
-            <script>
-                var modal = document.getElementById('open_news_1');
-                // When the user clicks anywhere outside of the modal, close it.
-                window.onclick = function(event) {
-                    if (event.target == modal) {
-                        modal.style.display = "none";
-                    }
-                }
-            </script> 
             <a class="block-news" onclick="document.getElementById('open_news_2').style.display='block'"> 
                 <img class="block-news-img" src="images/new_5.jpg" onError="this.src='images/not_found.png'">
                 <div class="block-news-title">Ученики Ametsuchi отдыхают в Анапе</div>
@@ -63,28 +54,19 @@
                     <div class="block-news-date">18.07.2022</div>
                 </div>
             </a>  
-        <div id="open_news_2" class="modal">
-            <span onclick="document.getElementById('open_news_2').style.display='none'" class="close" title="Close Modal">&times;</span>     
-            <div class="modal-news-content">
-                <img class="modal__img" src="images/new_5.jpg" onError="this.src='images/not_found.png'">
-                <div class="block__news">
-                    <div class="block-news-title">Ученики Ametsuchi отдыхают в Анапе</div>
-                    <div class="modal__news">
-                    <?php echo (file_get_contents('./all_news/new_5.txt')); ?>
+            <div id="open_news_2" class="modal">
+                <span onclick="document.getElementById('open_news_2').style.display='none'" class="close" title="Close Modal">&times;</span>     
+                <div class="modal-news-content">
+                    <img class="modal__img" src="images/new_5.jpg" onError="this.src='images/not_found.png'">
+                    <div class="block__news">
+                        <div class="block-news-title">Ученики Ametsuchi отдыхают в Анапе</div>
+                        <p class="modal__news">
+                            <?php echo (file_get_contents('./all_news/new_5.txt')); ?>
+                        </p>
                     </div>
-                </div>
+                </div> 
             </div> 
-        </div> 
-        <script>
-            var modal = document.getElementById('open_news_2');
-            // When the user clicks anywhere outside of the modal, close it.
-            window.onclick = function(event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
-        </script> 
-        <a class="block-news" onclick="document.getElementById('open_news_3').style.display='block'"> 
+            <a class="block-news" onclick="document.getElementById('open_news_3').style.display='block'"> 
                 <img class="block-news-img" src="images/new_4.jpg" onError="this.src='images/not_found.png'">
                 <div class="block-news-title">Ametsuchi посетил Сенсей Сергей Флоренко</div>
                 <div class="block-news-news">
@@ -98,61 +80,72 @@
                     <img class="modal__img" src="images/new_4.jpg" onError="this.src='images/not_found.png'">
                     <div class="block__news">
                         <div class="block-news-title">Ametsuchi посетил Сенсей Сергей Флоренко</div>
-                        <div class="modal__news">
+                        <p class="modal__news">
                             <?php echo (file_get_contents('./all_news/new_3.txt')); ?>
-                        </div>
+                        </p>
                     </div>
                 </div> 
             </div> 
-            <script>
-                var modal = document.getElementById('open_news_3');
-                // When the user clicks anywhere outside of the modal, close it.
-                window.onclick = function(event) {
-                    if (event.target == modal) {
-                        modal.style.display = "none";
-                    }
-                }
-            </script> 
-            <div class="block-news">
+            <a class="block-news" onclick="document.getElementById('open_news_4').style.display='block'"> 
                 <img class="block-news-img" src="images/new_1.jpg" onError="this.src='images/not_found.png'">
                 <div class="block-news-title">Аттестованы!</div>
                 <div class="block-news-news">
-                В конце мая в Казани прошла плановая аттестация учеников РОО Федерации айкидо Аметсучи по Республике Татарстан. 
-                                    <div class="block-news-date">27.05.2023</div>
-                </div>
-                <input class="block__hidden" type="checkbox" id="is_hidden_1">
-                <label for="is_hidden_1">Прочитать всё.</label>
-                <div class="hidden__news">
                     <?php echo (file_get_contents('./all_news/new_1.txt')); ?>
+                    <div class="block-news-date">27.05.2023</div>
                 </div>
-            </div>
-            <div class="block-news">
+            </a>  
+            <div id="open_news_4" class="modal">
+                <span onclick="document.getElementById('open_news_4').style.display='none'" class="close" title="Close Modal">&times;</span>     
+                <div class="modal-news-content">
+                    <img class="modal__img" src="images/new_1.jpg" onError="this.src='images/not_found.png'">
+                    <div class="block__news">
+                        <div class="block-news-title">Аттестованы!</div>
+                        <p class="modal__news">
+                            <?php echo (file_get_contents('./all_news/new_1.txt')); ?>
+                        </p>
+                    </div>
+                </div> 
+            </div> 
+            <a class="block-news" onclick="document.getElementById('open_news_5').style.display='block'"> 
                 <img class="block-news-img" src="images/new_2.jpg" onError="this.src='images/not_found.png'">
                 <div class="block-news-title">Семинар Сенсея Халидова!</div>
                 <div class="block-news-news">
-                    Прошел 4-й международный семинар Айкидо под руководством Сенсея Халидова!
+                    <?php echo (file_get_contents('./all_news/new_2.txt')); ?>
                     <div class="block-news-date">14.05.2023</div>
                 </div>
-                <input class="block__hidden" type="checkbox" id="is_hidden_2">
-                <label for="is_hidden_2">Прочитать всё.</label>
-                <div class="hidden__news">
-                    <?php echo (file_get_contents('./all_news/new_2.txt')); ?>
-                </div>
-            </div>
-            <div class="block-news">
+            </a>  
+            <div id="open_news_5" class="modal">
+                <span onclick="document.getElementById('open_news_5').style.display='none'" class="close" title="Close Modal">&times;</span>     
+                <div class="modal-news-content">
+                    <img class="modal__img" src="images/new_2.jpg" onError="this.src='images/not_found.png'">
+                    <div class="block__news">
+                        <div class="block-news-title">Семинар Сенсея Халидова!</div>
+                        <p class="modal__news">
+                            <?php echo (file_get_contents('./all_news/new_2.txt')); ?>
+                        </p>
+                    </div>
+                </div> 
+            </div> 
+            <a class="block-news" onclick="document.getElementById('open_news_6').style.display='block'"> 
                 <img class="block-news-img" src="images/new_3.jpg" onError="this.src='images/not_found.png'">
                 <div class="block-news-title">Ушел Великий Мастер...</div>
                 <div class="block-news-news">
-                    Федерация Айкидо Ametsuchi с прискорбием сообщает об уходе великого мастера Айкидо Ясунари Китаура.
+                    <?php echo (file_get_contents('./all_news/new_4.txt')); ?>
                     <div class="block-news-date">26.04.2022</div>
                 </div>
-                <input class="block__hidden" type="checkbox" id="is_hidden_4">
-                <label for="is_hidden_4">Прочитать всё.</label>
-                <div class="hidden__news">
-                    <?php echo (file_get_contents('./all_news/new_4.txt')); ?>
-                </div>
-            </div>
-        </div>
+            </a>  
+            <div id="open_news_6" class="modal">
+                <span onclick="document.getElementById('open_news_6').style.display='none'" class="close" title="Close Modal">&times;</span>     
+                <div class="modal-news-content">
+                    <img class="modal__img" src="images/new_3.jpg" onError="this.src='images/not_found.png'">
+                    <div class="block__news">
+                        <div class="block-news-title">Ушел Великий Мастер...</div>
+                        <p class="modal__news">
+                            <?php echo (file_get_contents('./all_news/new_4.txt')); ?>
+                        </p>
+                    </div>
+                </div> 
+            </div> 
         <div class="block-news-transitions">
             <div class="block-news-transitions-arrow">
                 < </div>
