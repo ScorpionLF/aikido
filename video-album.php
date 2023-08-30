7b325d50e2659ab569025b16e0cbbea62e532710
Liniza <fanilovna03@gmail.com>
2023-08-30 15:25:55 +0300

    Changed video style.

--- video-album.php
+++ video-album.php
@@ -26,7 +26,7 @@
                 <div class="title-block-line"></div>
             </div>
         </div>
-        <div class="album-title">Название альбома</div>
+        <!-- <div class="album-title">Название альбома</div> -->
         <div class="video-gallery">
         <!--     <div class="error">
                 <div class="block-error__title">Страница в разработке.</div>
@@ -42,12 +42,12 @@
                 <div class="img-box">
                     <!-- <img src="" onError="this.src='images/not_found.png'"> -->
                     <iframe class="iFrame" src="https://www.youtube.com/embed/lQTaeof0660" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                </div> 
-                <title class="video-title">Обращение Сенсея Халидова Рашида Бакиевича к ученикам федерации Ametsuchi Dojo</title>
+                <!-- <title class="video-title">Обращение Сенсея Халидова Рашида Бакиевича к ученикам федерации Ametsuchi Dojo</title> -->
             </a>                 
             <div id="open_video" class="modal">
                 <span onclick="document.getElementById('open_video').style.display='none'" class="close" title="Close Modal">&times;</span>     
                 <div class="modal-video-content">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/lQTaeof0660" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                </div>
+                <iframe  src="https://www.youtube.com/embed/lQTaeof0660" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                </div>
             </div> 
             <script>
                 var modal = document.getElementById('open_video');
